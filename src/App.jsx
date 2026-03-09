7caa2439e455a289593e151d4594a03369460a7a
hars-shit <upadhyayharshit05@gmail.com>
2026-03-09 21:44:47 +0530

    env added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { Toaster } from "sonner";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
-import Index from "./pages/Index";
-import User from "./pages/User";
-import Login from "./pages/Login";
 import MainLayout from "./layout/MainLayout";
 import ProtectedRoute from "./utils/ProtectedRoute";
+import { lazy, Suspense } from "react";
 
 const App = () => {
+
+const Index = lazy(() => import("./pages/Index"));
+const User = lazy(()=>import("./pages/User"));
+const Login=lazy(()=>import("./pages/Login"))
 
   const token = localStorage.getItem("access_token");
 
   return (
     <>
       <Toaster richColors position="top-right" />
-
+    <Suspense fallback={<div>Loading...</div>}>
       <BrowserRouter>
         <Routes>
           <Route
@@ -32,6 +34,7 @@ const App = () => {
 
         </Routes>
       </BrowserRouter>
+      </Suspense>
     </>
   );
 };
