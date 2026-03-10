adeae0d7761635fe73fcf2fc59fd02503bc8f2f0
hars-shit <upadhyayharshit05@gmail.com>
2026-03-10 18:42:41 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,38 @@ import MainLayout from "./layout/MainLayout";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import { lazy, Suspense } from "react";
 
-const App = () => {
-
 const Index = lazy(() => import("./pages/Index"));
-const User = lazy(()=>import("./pages/User"));
-const Login=lazy(()=>import("./pages/Login"))
+const User = lazy(() => import("./pages/User"));
+const Login = lazy(() => import("./pages/Login"));
 
+/* Login Guard */
+const LoginGuard = () => {
   const token = localStorage.getItem("access_token");
+  return token ? <Navigate to="/" replace /> : <Login />;
+};
 
+const App = () => {
   return (
     <>
       <Toaster richColors position="top-right" />
-    <Suspense fallback={<div>Loading...</div>}>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/login"
-            element={token ? <Navigate to="/" replace /> : <Login />
-              
-            }
-          />
 
-          {/* Protected Routes */}
-          <Route element={<ProtectedRoute />}>
-            <Route element={<MainLayout />}>
-              <Route path="/" element={<Index />} />
-              <Route path="/user/:id" element={<User />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <Routes>
+
+            {/* Login route */}
+            <Route path="/login" element={<LoginGuard />} />
+
+            {/* Protected Routes */}
+            <Route element={<ProtectedRoute />}>
+              <Route element={<MainLayout />}>
+                <Route path="/" element={<Index />} />
+                <Route path="/user/:id" element={<User />} />
+              </Route>
             </Route>
-          </Route>
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
       </Suspense>
     </>
   );
